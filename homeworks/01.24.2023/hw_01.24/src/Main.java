d27da19ee6e4ac863e74f5b29a89ade6d3b575c2
vzhev <v.zhevaga@gmail.com>
2023-01-27 23:26:54 +0100

    fixed hw 01.24

--- homeworks/01.24.2023/hw_01.24/src/Main.java
+++ homeworks/01.24.2023/hw_01.24/src/Main.java
@@ -4,19 +4,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Queue queue = new Queue(5);
+        System.out.println(queue.toString());
+        queue.remove();
         queue.pushToEnd(1);
         queue.pushToEnd(2);
         queue.pushToEnd(3);
         queue.pushToEnd(4);
         queue.pushToEnd(5);
-        System.out.println(queue.size());
-        System.out.println(queue.peek());
+        System.out.println("добавлено пять элементов");
+        System.out.println(queue.toString());
+        queue.pushToEnd(6);
+        System.out.println("size: " + queue.size());
+        System.out.println("первый элемент: " + queue.peek());
+        System.out.println("пусто?: " + queue.isEmpty());
         queue.remove();
         queue.remove();
         queue.remove();
         queue.remove();
         queue.remove();
-        System.out.println(queue.isEmpty());
-        System.out.println(queue.size());
+        System.out.println("удалено пять элементов");
+        System.out.println(queue.toString());
+        queue.remove();
+        System.out.println("пусто?: " + queue.isEmpty());
+        System.out.println("size: " + queue.size());
     }
 }
--- homeworks/01.24.2023/hw_01.24/src/Queue.java
+++ homeworks/01.24.2023/hw_01.24/src/Queue.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Queue {
     private int[] arr;      // массив для хранения элементов queue
     private int head;      // head указывает на передний элемент в queue
@@ -10,23 +12,64 @@ public class Queue {
         capacity = size;
 //        front = 0;
 //        rear = -1;
+        head = 0;
+        tail = -1;
         count = 0;
     }
 
     public void pushToEnd(int data){ // - вставляет элемент в конец queue
-        arr[count] = data;
-        count++;
+        // 1. массив пустой
+        //        count = 0;
+        //        head = 0;
+        //        tail = -1;
+        // 2. в массиве есть как минимум 1 значение
+        //        count = 1;
+        //        head = arr[0];
+        //        tail = arr[count];
+        // 3. массиве полный
+        //        count = capacity;
+        //        head = arr[0];
+        //        tail = arr[count];
+        if(count < capacity){
+            if (count == 0){
+                head = data;
+            }
+            tail = data;
+            arr[count] = data;
+            count++;
+        } else {
+            System.out.println("Попытка добавления => Ошибка переполнения. Массив полный");
+        }
     };
 
     public void remove(){ // - удаляет элемент из начала queue
-        if(count!=0){
+        // 1. массив пустой
+        //        count = 0;
+        //        head = 0;
+        //        tail = -1;
+        // 2. в массиве есть как минимум 1 значение
+        //        count = 1;
+        //        head = arr[0];
+        //        tail = arr[count];
+        // 3. массиве полный
+        //        count = capacity;
+        //        head = arr[0];
+        //        tail = arr[count];
+        if(count > 1){
             head = arr[1];
             for (int i = 1; i < count; i++) {
                 arr[i-1] = arr[i];
             }
             count--;
-            tail = arr[count];
+            tail = arr[count-1];
             arr[count] = 0;
+        } else if(count == 1){
+            head = 0;
+            tail = -1;
+            count--;
+            arr[count] = 0;
+        } else {
+            System.out.println("Попытка удаления => Ошибка. Массив пуст");
         }
     };
 
@@ -36,13 +79,24 @@ public class Queue {
 
     public boolean isEmpty() {
         if (count > 0) {
-            return true;
-        } else {
             return false;
+        } else {
+            return true;
         }
     }
 
     public int size(){
         return count;
     };
+
+    @Override
+    public String toString() {
+        return "Queue{" +
+                "arr=" + Arrays.toString(arr) +
+                ", head=" + head +
+                ", tail=" + tail +
+                ", capacity=" + capacity +
+                ", count=" + count +
+                '}';
+    }
 }
